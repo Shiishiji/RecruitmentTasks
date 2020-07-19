ebc02cfaa012a1e4c8e1f8c41ddb2b7a3f4e3e87
Siguard <dszopinski97@gmail.com>
2020-07-19 17:12:06 +0200

    Created unit tests for Palindrome class

--- src/com/company/PalindromeTest.java
+++ src/com/company/PalindromeTest.java
@@ -1,5 +1,43 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
 
 class PalindromeTest {
 
+    @Test
+    void shouldReturnAbba() {
+        Palindrome palindrome = new Palindrome();
+        String result = palindrome.solution(4,2);
+        Assert.assertEquals("abba", result);
+    }
+
+    @Test
+    void shouldReturnAbbba() {
+        Palindrome palindrome = new Palindrome();
+        String result = palindrome.solution(5,2);
+        Assert.assertEquals("abbba", result);
+    }
+
+    @Test
+    void shouldReturnAbbbba() {
+        Palindrome palindrome = new Palindrome();
+        String result = palindrome.solution(6, 2);
+        Assert.assertEquals("abbbba", result);
+    }
+
+    @Test
+    void shouldReturnAbccba() {
+        Palindrome palindrome = new Palindrome();
+        String result = palindrome.solution(6, 3);
+        Assert.assertEquals("abccba", result);
+    }
+
+    @Test
+    void shouldReturnAbcdcba() {
+        Palindrome palindrome = new Palindrome();
+        String result = palindrome.solution(7, 4);
+        Assert.assertEquals("abcdcba", result);
+    }
 }

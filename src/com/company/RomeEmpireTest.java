7f5af3a5b6dbb26bdf2e4936f6b95ac711a673d0
Siguard <dszopinski97@gmail.com>
2020-07-19 19:56:34 +0200

    Created unit tests for RomeEmpire class

--- src/com/company/RomeEmpireTest.java
+++ src/com/company/RomeEmpireTest.java
@@ -1,5 +1,59 @@
+package com.company;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RomeEmpireTest {
 
+    RomeEmpire romeEmpire;
+
+    @BeforeEach
+    void setUp() {
+        romeEmpire = new RomeEmpire();
+    }
+
+    @Test
+    void firstCase() {
+        int [] a = { 1, 2, 3 };
+        int [] b = { 0, 0, 0 };
+        try {
+            int result = romeEmpire.solution(a, b);
+            assertEquals(0, result);
+        } catch (Exception e) {
+            assertEquals(a.length,b.length);
+        }
+    }
+
+    @Test
+    void secondCase() {
+        int [] a = { 0, 1, 2, 4, 5 };
+        int [] b = { 2, 3, 3, 3, 2 };
+        try {
+            int result = romeEmpire.solution(a, b);
+            assertEquals(3, result);
+        } catch (Exception e) {
+            assertEquals(a.length,b.length);
+        }
+    }
+
+    @Test
+    void thirdCase() {
+        int [] a = { 2, 3, 3, 4 };
+        int [] b = { 1, 1, 0, 0 };
+        try {
+            int result = romeEmpire.solution(a, b);
+            assertEquals(-1, result);
+        } catch (Exception e) {
+            assertEquals(a.length,b.length);
+        }
+    }
+
+    @Test
+    void inputParametersLengthMismatch() {
+        int [] a = { 1, 2, 3 };
+        int [] b = { 4, 5 };
+        assertThrows(Exception.class, () -> { romeEmpire.solution(a, b); });
+    }
 }

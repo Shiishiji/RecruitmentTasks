27bf9cf14afe287da8ff0f13be703778c4b1a807
Siguard <dszopinski97@gmail.com>
2020-07-19 17:11:38 +0200

    Fixed error with bad palindromes

--- src/com/company/Palindrome.java
+++ src/com/company/Palindrome.java
@@ -5,18 +5,20 @@ public class Palindrome {
     char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
 
     public String solution(int N, int K) {
-        String palindrome = "";
-        for (int i=0, j=0; i<(N/2)-1; i++) {
-            if (i <= K)
+        StringBuilder palindrome = new StringBuilder();
+        for (int i=0, j=0; i<(N/2); i++) {
+            if (i <= K-1)
                 j = i;
 
-            palindrome += alphabet[j];
+            palindrome.append(alphabet[j]);
         }
 
-        if (N%2==1)
-            palindrome += this.alphabet[K-1];
-
-        palindrome += (new StringBuilder(palindrome).reverse().toString());
-        return palindrome;
+        if (N%2==1) {
+            palindrome.append(this.alphabet[K - 1]);
+            palindrome.append(new StringBuilder(palindrome.substring(0, palindrome.length() - 1)).reverse().toString());
+        } else {
+            palindrome.append(new StringBuilder(palindrome).reverse().toString());
+        }
+        return palindrome.toString();
     }
 }
